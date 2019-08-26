6a54a872a75afc00040ae593d9e60db37d196bc1
Jacob <jbo@brondby.dk>
2019-08-26 15:56:13 +0200

    jjj

---
+++ O14_Klasser_terning/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace O14_Klasser_terning
+{
+    public class Terning
+
+    {
+
+        private static Random rnd = new Random();
+
+        public int værdi;
+
+        private bool snyd;
+
+
+
+        public Terning()
+
+        {
+
+            this.værdi = 1;
+
+            this.snyd = false;
+
+        }
+
+
+
+        public Terning(bool snyd)
+
+        {
+
+            this.snyd = snyd;
+
+            Ryst();
+
+        }
+
+        public void Skriv()
+
+        {
+
+            Console.WriteLine("[" + this.værdi + "]");
+
+        }
+
+
+
+        public void Ryst()
+
+        {
+
+            if (snyd)
+
+                this.værdi = 6;
+
+            else
+
+                this.værdi = rnd.Next(1, 7);
+
+        }
+
+    }
+}
---
+++ O14_Klasser_terning/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+
+namespace O14_Klasser_terning
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            //Terning t1 = new Terning();
+
+            //t1.Skriv();
+
+            //t1.Ryst();
+
+            //t1.Skriv();
+
+
+
+            Terning t2 = new Terning(true);
+
+            t2.Skriv();
+
+            t2.Ryst();
+
+            t2.Skriv();
+
+
+
+
+
+            // Hold console åben ved debug (kun nødvendigt ved .NET Framework - ikke .NET Core)
+
+            if (System.Diagnostics.Debugger.IsAttached)
+
+            {
+
+                Console.Write("Press any key to continue . . . ");
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+
+}
