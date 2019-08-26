5187ebff9ec1e40570ca76da1b11aef5d5bce099
Jacob <jbo@brondby.dk>
2019-08-26 13:19:37 +0200

    1122

---
+++ O12_samlinger_generisk/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+
+
+namespace O12_samlinger_generisk
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+
+
+            List<int> lst1 = new List<int>();
+
+            lst1.Add(3);
+
+            lst1.Add(1);
+
+            lst1.Add(8);
+
+            foreach (var item in lst1)
+
+            {
+
+                Console.WriteLine(item);
+
+            }
+
+            System.Collections.Generic.HashSet<>
+
+            Dictionary<int, string> lst2 = new Dictionary<int, string>();
+
+            lst2.Add(1, "M");
+
+            lst2.Add(2, "T");
+
+            lst2.Add(3, "P");
+
+            var p = lst2[3];
+
+            Console.WriteLine(p);
+
+
+
+            // Hold console åben ved debug (kun nødvendigt ved .NET Framework - ikke .NET Core)
+
+            if (System.Diagnostics.Debugger.IsAttached)
+
+            {
+
+                Console.Write("Press any key to continue . . . ");
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+
+
+
+}
