db1af2f1d3f46e70cfab543a35418dd34f319603
Jacob <jbo@brondby.dk>
2019-08-26 14:46:56 +0200

    111

---
+++ O13_klasser_simpel/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+
+
+namespace O13_klasser_simpel
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+
+
+            Person p1 = new Person();
+
+            p1.Efternavn = "cronberg";
+
+            p1.Fornavn = "mathias";
+
+            p1.Fødselsår = 2006;
+
+            Console.WriteLine(p1.FuldtNavn());
+
+            Console.WriteLine(p1.EstimeretAlder());
+
+
+
+
+
+            Person p2 = new Person("mikkel", "cronberg", 2006);
+
+            Console.WriteLine(p2.FuldtNavn());
+
+            Console.WriteLine(p2.EstimeretAlder());
+
+
+
+            
+
+        }
+
+    }
+
+
+
+    class Person
+
+    {
+
+        public string Fornavn;
+
+        public string Efternavn;
+
+        public int Fødselsår;
+
+
+
+        public string FuldtNavn()
+
+        {
+
+            return $"{Fornavn} {Efternavn}";
+
+        }
+
+
+
+        public int EstimeretAlder()
+
+        {
+
+            int år = DateTime.Now.Year;
+
+            return år - this.Fødselsår;
+
+        }
+
+
+
+        // default constructor
+
+        public Person()
+
+        {
+
+            this.Fornavn = "";
+
+            this.Efternavn = "";
+
+        }
+
+
+
+        // Custom constructor
+
+        public Person(string fornavn, string efternavn, int fødselsår)
+
+        {
+
+            this.Fødselsår = fødselsår;
+
+            this.Fornavn = fornavn;
+
+            this.Efternavn = efternavn;
+
+        }
+
+    }
+
+}
+
