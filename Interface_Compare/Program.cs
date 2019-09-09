c2517175aeb98a5fe6131ebfacf020ac499e14a2
jbojacob <jbo@brondby.dk>
2019-09-09 13:24:37 +0200

    1122

---
+++ Interface_Compare/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Interface_Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[2];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn);
+                Console.WriteLine(item.Alder);
+            }
+
+
+            // Hold console åben ved debug (kun nødvendigt ved .NET Framework - ikke .NET Core)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    // class Hund : IComparable
+    class Hund : IComparable<Hund>
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        //public int CompareTo(object obj)
+        //{
+        //    Hund h = obj as Hund;
+        //    if (h.Alder > this.Alder)
+        //        return -1;
+        //    if (h.Alder < this.Alder)
+        //        return 1;
+        //    return 0;
+        //}
+
+        public int CompareTo(Hund h)
+        {
+            if (h.Alder > this.Alder)
+                return -1;
+            if (h.Alder < this.Alder)
+                return 1;
+            return 0;
+
+        }
+    }
+}
---
+++ Interface_eget/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+
+namespace Interface_eget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbFunktioner[] array = new IDbFunktioner[4];
+            array[0] = new Hund();
+            array[1] = new Ubåd();
+            array[2] = new Hund();
+            array[3] = new Ubåd();
+            foreach (var item in array)
+            {
+                item.Gem();
+                item.Skriv();
+            }
+
+            // Hold console åben ved debug (kun nødvendigt ved .NET Framework - ikke .NET Core)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+    }
+
+    interface IDbFunktioner
+    {
+        void Gem();
+        void Skriv();
+    }
+
+    class Hund : IDbFunktioner
+    {
+
+        public string Navn { get; set; }
+
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer hund (her kunne der uploades til en database - smart)...");
+            Console.WriteLine("");
+        }
+        public void Skriv()
+        {
+            Console.WriteLine("Her skrives der omkring hunden fx i logfil...");
+            Console.WriteLine("");
+        }
+
+    }
+
+    class Ubåd : IDbFunktioner
+    {
+        public int Nummer { get; set; }
+        public double Tubine { get; set; }
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer ubåd...");
+            Console.WriteLine("");
+        }
+        public void Skriv()
+        {
+            Console.WriteLine("Her skrives der omkring ubåden fx i logfil...");
+            Console.WriteLine("");
+        }
+    }
+
+}
