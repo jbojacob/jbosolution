1ac846263737026a566cecb8c5e8745a9e60c791
Jacob <admin@TA-8214-16>
2019-09-10 15:42:43 +0200

    Slut commit

---
+++ _Testing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Windows.Forms;
+
+namespace _Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryEntry directoryEntry = new DirectoryEntry("WinNT://" + Environment.MachineName);
+            string userNames = "Users: ";
+
+            foreach (DirectoryEntry child in directoryEntry.Children)
+            {
+                if (child.SchemaClassName == "User")
+                {
+                    userNames += child.Name + Environment.NewLine;
+                }
+
+            }
+            MessageBox.Show(userNames);
+        }
+
+        //private static SearchResultCollection GetAllUsers()
+        //{
+        //    Console.WriteLine();
+        //}
+    }
+}
---
+++ events_filewatcher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace events_filewatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.FileSystemWatcher w =
+                new System.IO.FileSystemWatcher(@"c:\temp", "*.*");
+            w.EnableRaisingEvents = true;
+            w.Changed += W_Changed;
+            w.Created += (s, e) => Console.WriteLine("Oprettet " + e.FullPath);
+            w.Deleted += (s, e) => Console.WriteLine("Slettet " + e.FullPath);
+
+            do { } while (true);
+
+            //System.IO.FileSystemWatcher d = new System.IO.FileSystemWatcher(@"c:\temp", "*.*");
+            //d.EnableRaisingEvents = true;
+            //d.Deleted += D_Deleted;
+            //d.Deleted += (s, e) => Console.WriteLine("Slettet " + e.FullPath);
+            //do { } while (true);
+
+           
+        }
+
+        //private static void D_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        //{
+        //    Console.WriteLine(e.Name);
+        //}
+
+        private static void W_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.Name);
+        }
+
+
+    }
+}
